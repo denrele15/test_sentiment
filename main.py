3408befd4f39c152fe825920e9822085c6724240
denrele15 <78202883+denrele15@users.noreply.github.com>
2021-01-30 11:59:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+import pandas
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+df = pandas.read_csv('data.csv', encoding= 'ISO-8859-1')
+
+print(df['comment_text'])
+lines2 = df['comment_text']
+
+sys.stdout=open("output.txt" , "w", encoding='ISO-8859-1')
+
+sid = SentimentIntensityAnalyzer()
+i = -1
+
+for sentence in lines2:
+     print( '     ' + i.__str__()  + '     ,'+ sentence )
+     i=i+1
+     ss = sid.polarity_scores(sentence)
+     for k in ss:
+        print( '{0}, {1}, '.format(k, ss[k]), end='')
+
