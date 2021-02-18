c40af51df6cabf03dae7935210276e49a74bed0d
denrele15 <78202883+denrele15@users.noreply.github.com>
2021-02-18 23:13:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,29 +5,30 @@ import sys
 import pandas
 import matplotlib.pyplot as plt
 
-
-
 nltk.download('vader_lexicon')
 
-df = pandas.read_csv('data.csv', encoding= 'ISO-8859-1')
+df = pandas.read_csv('data.csv', encoding='ISO-8859-1')
 print(df['comment_text'])
 lines2 = df['comment_text']
 
-sys.stdout=open("output.txt" , "w", encoding='ISO-8859-1')
+sys.stdout = open("output.txt", "w", encoding='ISO-8859-1')
 sid = SentimentIntensityAnalyzer()
 i = -1
 
 for sentence in lines2:
-    print('     ' + i.__str__() + '     ,' + sentence) #outputs sequential no of each line of sentence
+    print('     ' + i.__str__() + '     ,' + sentence)  # outputs sequential no of each line of sentence
     i = i + 1
     ss = sid.polarity_scores(sentence)
     for k in ss:
         print('{0}, {1},'.format(k, ss[k]), end='::   ')
 
-#fig, ax1 = plt.subplots()
-    #ax1.bar(k.keys(), ss[k].values())
-    #fig.autofmt_xdate()
+for sentence in lines2:
+            ss = sid.polarity_scores(sentence)
+            for k in ss:
+                plt.bar(k, ss[k])
+                plt.savefig('graph.png')
+                plt.show()
 
-    plt.bar(k, ss[k])
-    plt.savefig('graph.png')
-    plt.show()
+# fig, ax1 = plt.subplots()
+# ax1.bar(k.keys(), ss[k].values())
+# fig.autofmt_xdate()
