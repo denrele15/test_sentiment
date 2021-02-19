129917367ce08cbd1c409ea9096c0c669ba4cdae
denrele15 <78202883+denrele15@users.noreply.github.com>
2021-02-19 01:14:20 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# import nltk
-# from nltk.sentiment.vader import SentimentIntensityAnalyzer
-# import csv
-# import sys
-# import pandas
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import csv
+import sys
+import pandas
 
 import matplotlib.pyplot as plt
 
@@ -11,21 +11,21 @@ from matplotlib import style
 import time
 
 
-# nltk.download('vader_lexicon')
-# df = pandas.read_csv('data.csv', encoding='ISO-8859-1')
-# print(df['comment_text'])
-# lines2 = df['comment_text']
-#
-# sys.stdout = open("output.txt", "w", encoding='ISO-8859-1')
-# sid = SentimentIntensityAnalyzer()
-# i = -1
-#
-# for sentence in lines2:
-#     print('     ' + i.__str__() + '     ,' + sentence)  # outputs sequential no of each line of sentence
-#     i = i + 1
-#     ss = sid.polarity_scores(sentence)
-#     for k in ss:
-#         print('{0}, {1},'.format(k, ss[k]), end='::   ')
+nltk.download('vader_lexicon')
+df = pandas.read_csv('data.csv', encoding='ISO-8859-1')
+print(df['comment_text'])
+lines2 = df['comment_text']
+
+sys.stdout = open("output.txt", "w", encoding='ISO-8859-1')
+sid = SentimentIntensityAnalyzer()
+i = -1
+
+for sentence in lines2:
+    print('     ' + i.__str__() + '     ,' + sentence)  # outputs sequential no of each line of sentence
+    i = i + 1
+    ss = sid.polarity_scores(sentence)
+    for k in ss:
+        print('{0}, {1},'.format(k, ss[k]), end='::   ')
 
 
 style.use = ("ggplot")
